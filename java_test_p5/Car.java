e5e43dbc44034076a772bd59607d946634aac2c2
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-04 14:42:25 -0500

    Defined Car and Truck classes to extend Vehicle and inherit the class properties

--- java_test_p5/Car.java
+++ java_test_p5/Car.java
@@ -1,3 +1,6 @@
-public class Car {
-    
+public class Car extends Vehicle {
+    //call the constructor of the parent class
+    public Car(String make, String model, int year, int fuelCapacity) {
+        super(make, model, year, fuelCapacity);
+    }
 }
--- java_test_p5/Truck.java
+++ java_test_p5/Truck.java
@@ -1,3 +1,8 @@
-public class Truck {
-    
+//truck is a subclass of vehicle
+public class Truck extends Vehicle { 
+    //call the vehicle constructor, but with the truck subclass name so that the properties are inherited
+	public Truck(String make, String model, int year, int capacity) {
+		super(make, model, year, capacity); //super will call the constructor of the parent class
+	}
+	
 }
