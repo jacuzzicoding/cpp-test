00af9db086f18ea976b69c247f676195a300f3c9
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-04 16:54:42 -0500

    Engine is no longer a subclass

--- java_test_p5/Engine.java
+++ java_test_p5/Engine.java
@@ -1,6 +1,7 @@
-public class Engine extends Vehicle { //Engine is a subclass of Vehicle
-    public Engine(String make, String model, int year, int fuelCapacity) {
-        super(make, model, year, fuelCapacity);
+
+public class Engine { //Engine is its own class, should store cylinders
+   public Engine () { //constructor for the engine class
+     
     }
     
 }
