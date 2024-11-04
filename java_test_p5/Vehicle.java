b65e7a257040cc35dfc7d887f9ca8661561f3e84
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-04 16:55:55 -0500

    Made Vehicle an abstract class, and added engine as a property, as well as currentFuelLevel so we can track that

--- java_test_p5/Vehicle.java
+++ java_test_p5/Vehicle.java
@@ -2,13 +2,15 @@
 
 
 
-public class Vehicle {
+public abstract class Vehicle { //parent class, abstract because its the base class for all vehicle types
     
 /* PROPERTIES */
     private String make; // Property "make" to store the make of the vehicle
     private String model; // Property "model" to store the model of the vehicle
     private int year; // Property "year" to store the year of the vehicle
     private int fuelCapacity; // Property "fuelCapacity" to store the fuel capacity of the vehicle
+    private Engine engine; // Property "engine" to store the engine of the vehicle as its own object
+    private double currentFuelLevel; // Property "currentFuelLevel" to store the current fuel level of the vehicle
 
  /* GETTERS */
     public String getMake() { // Getter for make, these are the methods that will be used to give the values of the properties to the child classes
@@ -23,7 +25,12 @@ public class Vehicle {
     public int getFuelCapacity() {
         return fuelCapacity;
     }
-
+    public Engine getEngine() {
+        return engine;
+    }
+    public double getCurrentFuel() {
+        return currentFuelLevel;
+    }
 /* SETTERS */
     public void setMake(String make) {// Setter for make, these are the methods that will be used to set the values of the properties to the child classes
         this.make = make;
@@ -46,5 +53,12 @@ public class Vehicle {
         this.model = model;
         this.year = year;
         this.fuelCapacity = fuelCapacity;
+    }
+
+/* METHODS */
+    public void start() {
+        System.out.println("---Starting All Vehicles---");
+        //now we need to use the vehicle objects to start the vehicles
+        System.out.println("Starting the " + year + " " + make + " " + model + "...");
     }
 }
