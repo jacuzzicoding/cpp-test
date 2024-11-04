d58d1fa090c9cfdaefd6cbfbab5c1f3f0fbca987
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-04 14:12:11 -0500

    Moved child classes out of the vehicle class

--- java_test_p5/Vehicle.java
+++ java_test_p5/Vehicle.java
@@ -4,9 +4,42 @@
 
 public class Vehicle {
     
-    //here we will add the subclasses just to get it written down as part of the parent class
-    //might change this later but i'm still learning so i'm not sure yet
-    public class Truck extends Vehicle {
-        
+    private String make; // Property "make" to store the make of the vehicle
+    private String model; // Property "model" to store the model of the vehicle
+    private int year; // Property "year" to store the year of the vehicle
+    private int fuelCapacity; // Property "fuelCapacity" to store the fuel capacity of the vehicle
+    
+    // Getter for make
+    public String getMake() {
+        return make;
+    }
+    // Getter for model
+    public String getModel() {
+        return model;
+    }
+    // Getter for year
+    public int getYear() {
+        return year;
+    }
+    // Getter for fuelCapacity
+    public int getFuelCapacity() {
+        return fuelCapacity;
     }
 }
+    //here we will create the child classes for the different vehicle types
+    public class Truck extends Vehicle {
+        //code here
+    }
+    public class Car extends Vehicle {
+        //code here
+    }
+    public class Engine extends Vehicle {
+        //code here
+    }
+    public class Bike extends Vehicle {
+        //code here
+    }
+    public class main extends Vehicle {
+        //code here
+    }
+
