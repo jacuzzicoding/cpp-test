6522abb0c6c24443af5229e42ebc9058dcd2d1a9
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-04 14:42:32 -0500

    Refactor Vehicle class: added getters, setters, and constructor; improved code organization with comments

--- java_test_p5/Vehicle.java
+++ java_test_p5/Vehicle.java
@@ -4,31 +4,52 @@
 
 public class Vehicle {
     
+/* PROPERTIES */
     private String make; // Property "make" to store the make of the vehicle
     private String model; // Property "model" to store the model of the vehicle
     private int year; // Property "year" to store the year of the vehicle
     private int fuelCapacity; // Property "fuelCapacity" to store the fuel capacity of the vehicle
-    
-    // Getter for make
-    public String getMake() {
+
+ /* GETTERS */
+    public String getMake() { // Getter for make, these are the methods that will be used to give the values of the properties to the child classes
         return make;
     }
-    // Getter for model
     public String getModel() {
         return model;
     }
-    // Getter for year
     public int getYear() {
         return year;
     }
-    // Getter for fuelCapacity
     public int getFuelCapacity() {
         return fuelCapacity;
     }
+
+/* SETTERS */
+    public void setMake(String make) {// Setter for make, these are the methods that will be used to set the values of the properties to the child classes
+        this.make = make;
+    }
+    // Setter for model
+    public void setModel(String model) {
+        this.model = model;
+    }
+    // Setter for year
+    public void setYear(int year) {
+        this.year = year;
+    }
+    // Setter for fuelCapacity
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+/* CONSTRUCTOR */
+    public Vehicle(String make, String model, int year, int fuelCapacity) { //creating the constructor using the properties i declared above
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.fuelCapacity = fuelCapacity;
+    }
 }
-    //here we will create the child classes for the different vehicle types
+/* CHILD CLASSES // commented out because they are in separate files
     public class Truck extends Vehicle {
-        //code here
     }
     public class Car extends Vehicle {
         //code here
@@ -42,4 +63,4 @@ public class Vehicle {
     public class main extends Vehicle {
         //code here
     }
-
+*/
