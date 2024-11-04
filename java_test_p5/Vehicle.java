c82036af7bac1d8153f47eb8fcd29db592d6fc6b
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-04 14:03:35 -0500

    Started New Project in Java

---
+++ java_test_p5/Vehicle.java
@@ -0,0 +1,12 @@
+//this will be the parent class for all vehicle types
+
+
+
+public class Vehicle {
+    
+    //here we will add the subclasses just to get it written down as part of the parent class
+    //might change this later but i'm still learning so i'm not sure yet
+    public class Truck extends Vehicle {
+        
+    }
+}
