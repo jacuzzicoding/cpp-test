2841370c053fe180348eb74b807bc0b56c1b179e
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-04 15:36:22 -0500

    Added getModel() function to main

--- java_test_p5/main.java
+++ java_test_p5/main.java
@@ -1,19 +1,31 @@
 //main.java
 //this is the main class for the program that will be ran 
-
+/*it should:
+-prompt the user to enter the details of a new vehicle
+-ask if they want to enter another vehicle, 
+-if yes, prompt the user to enter the details of a new vehicle
+-if no, print the details of the vehicles entered
+    -details include: make, model, year, fuel capacity
+    -simulate "starting" the vehicle(s) and print the fuel level
+    -should allow user to refuel the vehicle(s) and print the fuel as being full
+    -simulate "stopping" the vehicle(s) and print the fuel level
+*/
 public class main {
-    //functions that main will call
-    public static String welcomeMsg = "Welcome to Brock's Garage!\n Enter details for a new vehicle.\n"; //welcome message to the user
-    public String getMake() {
+/*FUNCTIONS FOR MAIN */
+    public static String welcomeMsg = "Welcome to Tom's Garage!\n Enter details for a new vehicle.\n"; //welcome message to the user
+    public String getMake() { //function to get the make of the vehicle
         System.out.println("Enter make: "); //msg to the user to prompt them to enter the make
         String make = System.inlin.readLine(); //read the user input
+        return make; //return the make, so it is stored for the new vehicle
     }
+    public String getModel() { //function to get the model of the vehicle
+        System.out.println("Enter model: "); //msg to the user to prompt them to enter the model
+        String model = System.inlin.readLine(); //read the user input
+        return model; //return the model, so it is stored for the new vehicle
+    }
+
     public static void main(String[] args) {
         System.out.println(welcomeMsg); //print the welcome message to the user
     }
 }
-
-/* FUNCTIONS */
-
-//this function will be used to get the user input for the vehicle make
-
+\
