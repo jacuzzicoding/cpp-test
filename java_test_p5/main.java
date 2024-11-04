d641a9b5f70f19c11bdcb884c3cf00efd97b1ec3
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-04 15:24:32 -0500

    Added welcomeMsg() and getMake() functions to main.java

--- java_test_p5/main.java
+++ java_test_p5/main.java
@@ -1,4 +1,19 @@
-public class main extends Vehicle { //creating main as a subclass of Vehicle, might need to change it tho
-    //code will go here
-    
+//main.java
+//this is the main class for the program that will be ran 
+
+public class main {
+    //functions that main will call
+    public static String welcomeMsg = "Welcome to Brock's Garage!\n Enter details for a new vehicle.\n"; //welcome message to the user
+    public String getMake() {
+        System.out.println("Enter make: "); //msg to the user to prompt them to enter the make
+        String make = System.inlin.readLine(); //read the user input
+    }
+    public static void main(String[] args) {
+        System.out.println(welcomeMsg); //print the welcome message to the user
+    }
 }
+
+/* FUNCTIONS */
+
+//this function will be used to get the user input for the vehicle make
+
