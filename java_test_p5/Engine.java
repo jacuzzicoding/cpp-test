30bb63dbfe85e1f4343fa3b2753a230832a45170
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-19 23:35:48 -0500

    Make numCylinders property final in Engine class for immutability.

--- java_test_p5/Engine.java
+++ java_test_p5/Engine.java
@@ -1,15 +1,15 @@
 //the engine class is a separate class that will be used to create the engine object for the vehicle class
 public class Engine {
     /* PROPERTIES */
-    private int numCylinders; //need to take in the number of cylinders for the engine
+    private final int numCylinders; //made final because the number of cylinders should not change
     private String engineType; //and what type of engine it is
 
     /* CONSTRUCTOR */
     public Engine(int numCylinders, String engineType) {  //constructor for the engine class, taking in the number of cylinders and the type of engine
         this.numCylinders = numCylinders; //initializing the number of cylinders
         this.engineType = engineType; //initializing the type of engine
-    }
-
+    } 
+    
     /* METHODS */
     public void start() { //method to start the engine, will print out a message saying the engine has started
         System.out.println("Engine with " + numCylinders + " cylinders starts.");
