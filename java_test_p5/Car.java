bb76e0cfbaff85f2088c2092a4ffbd79736967a8
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-19 23:17:58 -0500

    Enhance Car class with air conditioning feature; added property to track AC status, updated constructor to include engine, and implemented toggleAC and overridden start methods.

--- java_test_p5/Car.java
+++ java_test_p5/Car.java
@@ -1,6 +1,22 @@
 public class Car extends Vehicle {
-    //call the constructor of the parent class
-    public Car(String make, String model, int year, int fuelCapacity) {
-        super(make, model, year, fuelCapacity);
+    // One unique property for Car
+    private boolean hasAirConditioning;
+    
+    public Car(String make, String model, int year, int fuelCapacity, Engine engine) {
+        super(make, model, year, fuelCapacity, engine);
+        this.hasAirConditioning = true;  // Most modern cars have AC
+    }
+    
+    // One unique method for Car
+    public void toggleAC() {
+        hasAirConditioning = !hasAirConditioning; //if the AC is on, turn it off. if it is off, turn it on
+        System.out.println("AC is now " + (hasAirConditioning ? "on" : "off")); //print out the status of the AC depending on the value of hasAirConditioning
+    }
+    
+    @Override
+    public void start() {
+        System.out.println("Car starts smoothly.");
+        System.out.println(getMake() + " " + getModel() + " starts.");
+        getEngine().start();
     }
 }
