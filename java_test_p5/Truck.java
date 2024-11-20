861263aca65da7d0d819e0cb5e7942f58ebf9aa1
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-19 23:18:10 -0500

    Enhance Truck class with trailer functionality; added property to track trailer status, updated constructor to include engine, and implemented unique start and toggleTrailer methods.

--- java_test_p5/Truck.java
+++ java_test_p5/Truck.java
@@ -1,8 +1,29 @@
 //truck is a subclass of vehicle
-public class Truck extends Vehicle { 
-    //call the vehicle constructor, but with the truck subclass name so that the properties are inherited
-	public Truck(String make, String model, int year, int capacity) {
-		super(make, model, year, capacity); //super will call the constructor of the parent class
-	}
-	
+public class Truck extends Vehicle {
+    /* PROPERTIES */
+    private boolean isTrailerAttached; // Property to track if a trailer is attached
+    
+    /* CONSTRUCTOR */
+    public Truck(String make, String model, int year, int fuelCapacity, Engine engine) {
+        super(make, model, year, fuelCapacity, engine);
+        this.isTrailerAttached = false; // Trucks start with no trailer
+    }
+    
+    /* METHODS */
+    @Override
+    public void start() {
+        System.out.println("Truck starts with a heavy roar."); //unique starting msg for trucks
+        System.out.println(getMake() + " " + getModel() + " starts.");
+        getEngine().start();
+    }
+
+    public void loadCargo() {
+        System.out.println("Loading cargo into " + getMake() + " " + getModel());
+    }
+    
+    // Unique method for trucks to attach/detach trailer
+    public void toggleTrailer() {
+        isTrailerAttached = !isTrailerAttached;
+        System.out.println("Trailer " + (isTrailerAttached ? "attached to" : "detached from") + " " + getMake() + " " + getModel());
+    }
 }
