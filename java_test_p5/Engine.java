43e154d9ba15c0ef213a21d0e46acb730a82eed9
Brock Jenkinson <brockjenkinson@gmail.com>
2024-11-19 23:18:04 -0500

    Refactor Engine class for improved clarity; updated constructor and method comments for better understanding of properties and functionality.

--- java_test_p5/Engine.java
+++ java_test_p5/Engine.java
@@ -1,26 +1,22 @@
 //the engine class is a separate class that will be used to create the engine object for the vehicle class
 public class Engine {
+    /* PROPERTIES */
     private int numCylinders; //need to take in the number of cylinders for the engine
     private String engineType; //and what type of engine it is
 
-/*CONSTRUCTOR */
-    public Engine(int numCylinders, String engineType) { 
-        this.numCylinders = numCylinders;
-        this.engineType = engineType;
+    /* CONSTRUCTOR */
+    public Engine(int numCylinders, String engineType) {  //constructor for the engine class, taking in the number of cylinders and the type of engine
+        this.numCylinders = numCylinders; //initializing the number of cylinders
+        this.engineType = engineType; //initializing the type of engine
     }
-/*METHODS */
-    public void start() {
+
+    /* METHODS */
+    public void start() { //method to start the engine, will print out a message saying the engine has started
         System.out.println("Engine with " + numCylinders + " cylinders starts.");
     }
 
-    public void stop() {
-        System.out.println("Engine stops.");
-    }
-
-    @Override //
-    public String toString() {
+    @Override
+    public String toString() { //override the toString method to print out the engine details in a nice format
         return numCylinders + " Cylinder " + engineType;
     }
-
-    
 }
